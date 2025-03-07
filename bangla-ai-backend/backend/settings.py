7dd23832bde2f6887133ae8e948873a754fd95b4
mrjisan12 <mizanurrahmanjisan12@gmail.com>
2025-03-07 17:17:21 +0600

    Upload pdf fixed v2

--- bangla-ai-backend/backend/settings.py
+++ bangla-ai-backend/backend/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#2l(!)5no*p^e2q^^p5x&t(o)j8f(=yk^1ov5a(l&#1*8_l451'
+SECRET_KEY = 'Security_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
