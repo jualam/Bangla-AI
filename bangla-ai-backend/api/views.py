c5771ca3a76c97bc4700957e2a314843a1c0fe6a
mrjisan12 <mizanurrahmanjisan12@gmail.com>
2025-03-22 17:57:25 +0600

    using tiktoken

--- bangla-ai-backend/api/views.py
+++ bangla-ai-backend/api/views.py
@@ -5,6 +5,8 @@ import openai
 from django.conf import settings
 import re
 
+# Original Code
+
 # # function for chunking
 # def split_text_into_chunks(text, max_tokens=1500):
 #     sentences = re.split(r'(?<=[।.!?]) +', text)  
@@ -23,10 +25,41 @@ import re
 
 #     return chunks
 
+# New Code for testing
+
+# def split_text_into_chunks(text, max_tokens=2500):
+#     # Split by sentences
+#     sentences = re.split(r'(?<=[।.!?]) +', text)
+#     chunks = []
+#     current_chunk = []
+#     current_tokens = 0
+
+#     for sentence in sentences:
+#         # Estimate the number of tokens in the sentence
+#         num_tokens = len(sentence.split())  # Rough estimate based on word count
+#         if current_tokens + num_tokens <= max_tokens:
+#             current_chunk.append(sentence)
+#             current_tokens += num_tokens
+#         else:
+#             chunks.append(" ".join(current_chunk))
+#             current_chunk = [sentence]
+#             current_tokens = num_tokens
+
+#     if current_chunk:
+#         chunks.append(" ".join(current_chunk))
+
+#     return chunks
 
 
-def split_text_into_chunks(text, max_tokens=1500):
-    # Split by sentences
+import tiktoken  # For token count estimation
+
+tokenizer = tiktoken.get_encoding("gpt2")
+
+def get_token_count(text):
+    """Returns the number of tokens for the input text."""
+    return len(tokenizer.encode(text))
+
+def split_text_into_chunks(text, max_tokens=2500):
     sentences = re.split(r'(?<=[।.!?]) +', text)
     chunks = []
     current_chunk = []
@@ -34,19 +67,21 @@ def split_text_into_chunks(text, max_tokens=1500):
 
     for sentence in sentences:
         # Estimate the number of tokens in the sentence
-        num_tokens = len(sentence.split())  # Rough estimate based on word count
-        if current_tokens + num_tokens <= max_tokens:
+        sentence_tokens = get_token_count(sentence)
+        if current_tokens + sentence_tokens <= max_tokens:
             current_chunk.append(sentence)
-            current_tokens += num_tokens
+            current_tokens += sentence_tokens
         else:
+            # If adding the sentence exceeds the limit, store the current chunk and start a new one
             chunks.append(" ".join(current_chunk))
             current_chunk = [sentence]
-            current_tokens = num_tokens
+            current_tokens = sentence_tokens
 
     if current_chunk:
         chunks.append(" ".join(current_chunk))
 
     return chunks
+
 
 
 
@@ -96,7 +131,10 @@ def translate_text_en_bn(request):
         
 
 
-    
+
+
+
+###################################################################
     
 @api_view(['POST'])
 def translate_text_bn_en(request):
